6a9b30b7f998ebc3ebc67101beba33ef80454fba
Kamsi-yonna <etiobikamsi@gmail.com>
2025-01-19 23:26:03 +0100

    update - made the view button stick to the bottom

--- components/LatestCommitCard.vue
+++ components/LatestCommitCard.vue
@@ -3,7 +3,7 @@
         class="p-2 md:p-4 gap-2 shadow-md max-w-[500px] w-full mx-auto border-[1px] border-gray-600 rounded-md cursor-default">
         <h2 class="text-lg font-semibold mb-2">Latest Commit</h2>
         <div v-if="gitUser" class="block p-2.5 w-full text-sm rounded-lg border border-gray-300">
-            <div v-if="latestPushEvent" class="flex items-start justify-between gap-4">
+            <div v-if="latestPushEvent" class="flex justify-between gap-4">
                 <div class="flex flex-col gap-2 max-w-[60%]">
                     <div class="flex items-center gap-2">
                         <img class="rounded-full h-4 w-4" :src="props.gitUser.avatar" :alt="props.gitUser.username" />
