33ed9cbd96eb4be3607a0d07750aaa7e3504f03b
Kamsi-yonna <etiobikamsi@gmail.com>
2025-01-24 02:16:10 +0100

    fix - build errors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,5 +35,7 @@ export default defineNuxtConfig({
       clientId: process.env.NUXT_GITHUB_CLIENT_ID,
       clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET
     }
-  }
+  },
+
+  compatibilityDate: '2025-01-24'
 })
