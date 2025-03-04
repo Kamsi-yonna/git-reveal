3da798d5a2b3ca1aafe9a93d0d367fdac39d0a5a
Kamsi-yonna <etiobikamsi@gmail.com>
2025-03-04 01:19:22 +0100

    feat - setup theuser analysis flow

--- components/UserAnalysis.vue
+++ components/UserAnalysis.vue
@@ -3,14 +3,7 @@
         class="p-2 md:p-4 shadow-md max-w-[500px] w-full mx-auto border-[1px] border-gray-600 rounded-md transition-[padding] ease-out">
 
         <span rows="4" class="block p-2.5 w-full text-sm  rounded-lg border border-gray-300  overflow-auto"
-            placeholder="Write your thoughts here...">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihilorem ipsum dolor sit
-            amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem
-            praesentium nihil.orem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores
-            et perferendis eaque, exercitationem praesentium nihil.orem ipsum dolor sit amet, consectetur adipisicing
-            elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.orem ipsum
-            dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium.
+            placeholder="Write your thoughts here..."> {{ analysis }}
         </span>
     </main>
 </template>
@@ -19,7 +12,9 @@
 import { defineProps } from 'vue';
 import type { GitHubUser } from '~/types/user';
 
+// Define props
 const props = defineProps<{
-    gitUser: GitHubUser | null; // Allow null
+    gitUser: GitHubUser | null;
+    analysis: string;
 }>();
 </script>
